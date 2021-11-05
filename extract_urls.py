80f45c2244c088ee01536217b19a9766fe8cc8e8
Emily Escamilla <vogt.em@gmail.com>
2021-11-05 16:22:37 +0000

    File with all of the extracted URLs

--- extract_urls.py
+++ extract_urls.py
@@ -10,9 +10,14 @@ import requests
 from extractor import Extractor
 from util import APIUtil
 
+f = open("0704_urls.txt", "w")
+
 dir_path = "/home/mklein/pdf/0704/"
 for filename in os.listdir(dir_path):
     pdf_path = os.path.join(dir_path, filename)
     extractor = Extractor()
     urls = extractor.extract_all_urls(pdf_path)
-    print(urls)
+    for url in urls:
+        f.write(url + '\n')
+
+f.close
