5a666fa7539ba51400a89600db6769b9bd391887
Emily Escamilla <vogt.em@gmail.com>
2021-11-05 15:39:08 +0000

    Added iteration through directory

--- extract_urls.py
+++ extract_urls.py
@@ -10,7 +10,9 @@ import requests
 from extractor import Extractor
 from util import APIUtil
 
-pdf_path = "/home/mklein/pdf/0704/0704.4003v9.pdf"
-extractor = Extractor()
-urls = extractor.extract_all_urls(pdf_path)
-print(urls)
+dir_path = "/home/mklein/pdf/0704/"
+for filename in os.listdir(dir_path):
+    pdf_path = os.path.join(dir_path, filename)
+    extractor = Extractor()
+    urls = extractor.extract_all_urls(pdf_path)
+    print(urls)
