0d3f51cdccd389af3c9696fcf090b4a3eff387d7
Emily Escamilla <vogt.em@gmail.com>
2021-11-12 22:53:19 +0000

    Created program to iterate through all files in a given directory and only record the last/highest version. The output is in files.txt. This will be added to the extract_urls flow to only cpature the last version of a PDF

---
+++ latest_version.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+f = open("files.txt", "w")
+dir_path = "/home/mklein/pdf/0704/"
+prev_file_base = ""
+prev_version = ""
+file_list = os.listdir(dir_path)
+for file_name in sorted(file_list):
+    parsed_file = re.findall(r"(\d{4}.\d*)v(\d*)", file_name)[0]
+    file_base = parsed_file[0]
+    if prev_file_base != file_base:
+        if prev_file_base == "":
+            prev_file_base = file_base
+        else:
+            f.write("Run for " + prev_file_base + "v" + prev_version + "\n")
+            prev_file_base = file_base
+    prev_version = parsed_file[1]
+f.write("Run for " + prev_file_base + "v" + prev_version + "\n")
