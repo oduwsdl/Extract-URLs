8229e54b21b74ee539b8c5a7ff2cc35035236bbc
Emily Escamilla <vogt.em@gmail.com>
2021-11-15 01:48:03 +0000

    Modified to add .pdf to the end of each file name

--- latest_version.py
+++ latest_version.py
@@ -7,13 +7,13 @@ prev_file_base = ""
 prev_version = ""
 file_list = os.listdir(dir_path)
 for file_name in sorted(file_list):
-    parsed_file = re.findall(r"(\d{4}.\d*)v(\d*)", file_name)[0]
+    parsed_file = re.findall(r"(\d{4}.\d*)v(\d*).pdf", file_name)[0]
     file_base = parsed_file[0]
     if prev_file_base != file_base:
         if prev_file_base == "":
             prev_file_base = file_base
         else:
-            f.write("Run for " + prev_file_base + "v" + prev_version + "\n")
+            f.write("Run for " + prev_file_base + "v" + prev_version + ".pdf\n")
             prev_file_base = file_base
     prev_version = parsed_file[1]
-f.write("Run for " + prev_file_base + "v" + prev_version + "\n")
+f.write("Run for " + prev_file_base + "v" + prev_version + ".pdf\n")
