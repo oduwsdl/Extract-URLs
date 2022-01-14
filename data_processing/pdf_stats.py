ea70ee91fb54beedfdf6616dd651390cfc091c89
Emily Vogt <elvogt@liberty.edu>
2022-01-14 13:13:41 -0500

    Modified pdf_stats.py to find stats for publications that reference a given platform. Answers the question 'Of the publications that reference a platform, what is the average number of references?'

--- data_processing/pdf_stats.py
+++ data_processing/pdf_stats.py
@@ -10,7 +10,7 @@ repo_file = open("./repo_results/all_repo_urls.json")
 repo_json = json.load(repo_file)
 repo_file.close()
 
-csv_file = open("pdf_stats.csv", "w")
+csv_file = open("./data_processing/pdf_stats.csv", "w")
 csvwriter = csv.writer(csv_file)
 csvwriter.writerow(['Directory', 'Category', 'Mean', 'Median', 'StDev', 'Cut1', 'Cut2', 'Cut3', 'Percentage'])
 
@@ -28,21 +28,45 @@ for dir in repo_json:
     gl_count = 0
     bb_count = 0
     for file in repo_json[dir]["files"]:
-        sf.append(repo_json[dir]["files"][file]["sourceforge"]["url_count"])
-        gh.append(repo_json[dir]["files"][file]["github"]["url_count"])
-        gl.append(repo_json[dir]["files"][file]["gitlab"]["url_count"])
-        bb.append(repo_json[dir]["files"][file]["bitbucket"]["url_count"])
+        # sf.append(repo_json[dir]["files"][file]["sourceforge"]["url_count"])
+        # gh.append(repo_json[dir]["files"][file]["github"]["url_count"])
+        # gl.append(repo_json[dir]["files"][file]["gitlab"]["url_count"])
+        # bb.append(repo_json[dir]["files"][file]["bitbucket"]["url_count"])
         if repo_json[dir]["files"][file]["sourceforge"]["url_count"] != 0:
             sf_count = sf_count + 1
+            sf.append(repo_json[dir]["files"][file]["sourceforge"]["url_count"])
         if repo_json[dir]["files"][file]["github"]["url_count"] != 0:
             gh_count = gh_count + 1
+            gh.append(repo_json[dir]["files"][file]["github"]["url_count"])
         if repo_json[dir]["files"][file]["gitlab"]["url_count"] != 0:
             gl_count = gl_count + 1
+            gl.append(repo_json[dir]["files"][file]["gitlab"]["url_count"])
         if repo_json[dir]["files"][file]["bitbucket"]["url_count"] != 0:
             bb_count = bb_count + 1
+            bb.append(repo_json[dir]["files"][file]["bitbucket"]["url_count"])
         file_count = file_count + 1
-    csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "SourceForge", s.mean(sf), s.median(sf), s.pstdev(sf)] + s.quantiles(sf, n=4) + [sf_count/file_count])
-    csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "GitHub", s.mean(gh), s.median(gh), s.pstdev(gh)] + s.quantiles(gh, n=4) + [gh_count/file_count])
-    csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "GitLab", s.mean(gl), s.median(gl), s.pstdev(gl)] + s.quantiles(gl, n=4) + [gl_count/file_count])
-    csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "Bitbucket", s.mean(bb), s.median(bb), s.pstdev(bb)] + s.quantiles(bb, n=4) + [bb_count/file_count])
+    if sf_count == 0:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "SourceForge", 0, 0, 0, 0, 0, 0, 0])
+    elif sf_count == 1:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "SourceForge", sf[0], sf[0], sf[0], sf[0], sf[0], sf[0], sf_count/file_count])
+    else:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "SourceForge", s.mean(sf), s.median(sf), s.pstdev(sf)] + s.quantiles(sf, n=4) + [sf_count/file_count])
+    if gh_count == 0:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "GitHub", 0, 0, 0, 0, 0, 0, 0])
+    elif gh_count == 1:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "GitHub", gh[0], gh[0], gh[0], gh[0], gh[0], gh[0], gh_count/file_count])
+    else:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "GitHub", s.mean(gh), s.median(gh), s.pstdev(gh)] + s.quantiles(gh, n=4) + [gh_count/file_count])
+    if gl_count == 0:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "GitLab", 0, 0, 0, 0, 0, 0, 0])
+    elif gl_count == 1:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "GitLab", gl[0], gl[0], gl[0], gl[0], gl[0], gl[0], gl_count/file_count])
+    else:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "GitLab", s.mean(gl), s.median(gl), s.pstdev(gl)] + s.quantiles(gl, n=4) + [gl_count/file_count])
+    if bb_count == 0:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "Bitbucket", 0, 0, 0, 0, 0, 0, 0])
+    elif bb_count == 1:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "Bitbucket", bb[0], bb[0], bb[0], bb[0], bb[0], bb[0], bb_count/file_count])
+    else:
+        csvwriter.writerow(["20" + dir[0:2] + "-" + dir[2:], "Bitbucket", s.mean(bb), s.median(bb), s.pstdev(bb)] + s.quantiles(bb, n=4) + [bb_count/file_count])
 csv_file.close()
