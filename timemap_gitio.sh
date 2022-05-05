7930633eb6efc722eaf91af7d35ad7541904b255
Emily Escamilla <vogt.em@gmail.com>
2022-05-05 17:50:28 +0000

    Retrieved TimeMaps for all 610 git.io shortened URLs

---
+++ timemap_gitio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while read u; do
+    md5ofurl=`echo $u | md5sum | awk '{print $1}'`
+    curl -s https://memgator.cs.odu.edu/timemap/json/$u > timemap/$md5ofurl.txt
+    sleep 10
+    echo $u timemap/$md5ofurl.txt >> timemap_map.txt
+done < git.io.txt
