5bb5258c5da72b1b5e21e0631a13de51a06f751b
Emily Vogt <elvogt@liberty.edu>
2022-07-05 13:42:21 -0400

    Added timemap_map.txt and timemap_results.csv and modified timemap_surt.sh to run this on the server

--- timemap_surt.sh
+++ timemap_surt.sh
@@ -3,11 +3,11 @@ md5ofurl=`echo $1 | md5sum | awk '{print $1}'`
 FILE=timemap/$md5ofurl.txt
 if [ -f "$FILE" ]; then
     if [ "$3" != "skip" ]; then
-        curl -A "MemGator evogt001@odu.edu" -s http://localhost:1208/timemap/json/$1 > $FILE
+        curl -A "MemGator evogt001@odu.edu" -s https://memgator.cs.odu.edu/timemap/json/$1 > $FILE
         sleep 10
     fi
 else 
-    curl -A "MemGator evogt001@odu.edu" -s http://localhost:1208/timemap/json/$1 > $FILE
+    curl -A "MemGator evogt001@odu.edu" -s https://memgator.cs.odu.edu/timemap/json/$1 > $FILE
     echo $1 $2 $FILE >> timemap_map.txt
     sleep 3
 fi
