61394828b186bd62f19096d1350ee39b3b8d5076
Emily Vogt <elvogt@liberty.edu>
2023-01-20 11:24:18 -0500

    Deleted the partial results files because the same contents are found in the compiled files. Compiled data_processing/swh_curl_map[0-2].csv into data_processing/dedupe_curl_map.csv. I ran curl_results.py on the compiled file to generate data_processing/curl_results.csv

--- curl_results.py
+++ curl_results.py
@@ -6,28 +6,33 @@ http_404 = 0
 http_200 = 0
 http_none = 0
 http_other = 0
-with open('data_processing/test_curl_map.csv', newline='') as map_file:
+with open('data_processing/dedupe_curl_map.csv', newline='') as map_file:
     curl_map = csv.reader(map_file, delimiter=',')
     for row in curl_map:
         url = row[0]
+        ghp = row[1]
+        repo_url = row[2]
         file_name = row[3]
-        with open(file_name, 'r') as f:
-            status = ""
-            for line in f:
-                http_code = re.findall(r"HTTP\/.{1,3} (\d*)", line)
-                if len(http_code) != 0:
-                    status = http_code[0]
-                l = re.findall(r"^[Ll]ocation: (.*)$", line)
-            if l != []:
-                location = l[0]
-            else:
-                location = ''
-            if status == "404":
-                http_404 = http_404 + 1
-            elif status == "200":
-                http_200 = http_200 + 1
-            elif status == "":
-                http_none = http_none + 1
-            else: 
-                http_other = http_other + 1
-            print(url + " " + location + " " + status)
+        try: 
+            with open(file_name, 'r') as f:
+                status = ""
+                location = ""
+                for line in f:
+                    http_code = re.findall(r"HTTP\/.{1,3} (\d*)", line)
+                    if len(http_code) != 0:
+                        status = http_code[0]
+                    l = re.findall(r"^[Ll]ocation: (.*)$", line)
+                    if len(l) != 0:
+                        location = l[0]
+                if status == "404":
+                    http_404 = http_404 + 1
+                elif status == "200":
+                    http_200 = http_200 + 1
+                elif status == "":
+                    http_none = http_none + 1
+                else: 
+                    http_other = http_other + 1
+        except:
+            location = "DNE"
+            status = "DNE"
+        print(url + "," + ghp + "," + repo_url + "," + file_name + "," + location + "," + status)
