e806f230541830df37043cc27f531e7eef39f543
cs_evogt001 <cs_evogt001@terra.research.cs.odu.edu>
2023-01-19 21:52:58 -0500

    Removed duplicate file names caused by capitalization differences. Modified remove_duplicate_case.sh to move/rename the file to an all-lowercase version if there wasn't already one instead of the original script that only removed capitalized files if there was an all-lowercase version

--- remove_duplicate_case.sh
+++ remove_duplicate_case.sh
@@ -5,8 +5,9 @@ typeset -l l
 for f in $1/*
 do 
   l=$f
-  if [ "$l" != "$f" -a -e "$l" ]
-  then 
+  if [ "$l" != "$f" -a -e "$l" ]; then 
     rm "$f"
+  elif [ "$l" != "$f" ]; then
+    mv $f $l 
   fi
 done
