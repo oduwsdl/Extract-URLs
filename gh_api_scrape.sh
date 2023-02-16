9fc2ef0e1e5ac13ec0f90cffd7b546757b4ef004
Emily Vogt <elvogt@liberty.edu>
2023-02-16 12:33:40 -0500

    WIP. Added gh_timemap.csv which is a file of repo URLs that did not previously get a timemap. Added gh_repo_timemap.py to iterate through the CSV and call timemap_surt.sh

--- gh_api_scrape.sh
+++ gh_api_scrape.sh
@@ -4,20 +4,20 @@ URL=$1
 RESET=$2
 REMAINING=$3
 TOKEN=$6
-if [ ! -f "$4" ]; then
+if [ ! -f "$5" ]; then
     if (( REMAINING == 0 )); then
         NOW=$(date +%s)
         TIME=$(( RESET - NOW + 1 ))
         sleep $TIME
     fi
 
-    RESP=$(curl -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" -H "Accept: application/vnd.github+json" -is "$URL")
+    RESP=$(curl -L -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" -H "Accept: application/vnd.github+json" -is "$URL")
     # RESP=$(curl -H "X-GitHub-Api-Version: 2022-11-28" -H "Accept: application/vnd.github+json" -is "$URL")
     HEADERS=$(echo "$RESP" | sed '/^\r$/q')
-    echo $HEADERS >> $4
+    echo $RESP >> $4
     REMAINING=$(echo "$HEADERS" | sed -n -E 's/x-ratelimit-remaining: ([0-9]+)/\1/p' | tr -d '\r')
     RESET=$(echo "$HEADERS" | sed -n -n -E 's/x-ratelimit-reset: ([0-9]+)/\1/p' | tr -d '\r')
-    PULL=$(echo "$RESP" | sed '1,/^\r$/d')
+    PULL=$(echo "$RESP" | sed -n '/^$/{g;D;}; N; $p;')
     echo $PULL >> $5
     echo $RESET $REMAINING
 else
