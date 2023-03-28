4bfee81e3c12a92ff78f932c7bf25a9b9fb4e7dd
Emily Vogt <elvogt@liberty.edu>
2023-03-28 09:56:35 -0400

    Added results from classifier_results/classifier_results_json.py that are in raw_data_outputs/classifier_results/

--- classifier_results/classifier_results_json.py
+++ classifier_results/classifier_results_json.py
@@ -1,36 +1,44 @@
 import csv
 import json
-
-# For file in file_list:
-# input_file = file
-# output_file = file[:-4] + ".json"
+import re
 
 input_file = "parsed_Arxiv-Output.csv"
 output_file = "parsed_Arxiv-Output.json"
 
-# Change location of JSON files
-d = open("./classifier_results/" + output_file, "w")
+# d = open("./classifier_results/" + output_file, "w")
+
+def get_dir(filename):
+    return re.findall(r"(\d{4}).\d*v\d*.pdf", filename)[0]
+    
 
 with open("./classifier_results/" + input_file, newline='') as classifier_results_file:
     classifier_results = csv.reader(classifier_results_file, delimiter=',')
 
-    # Derive the directory and replace dir
-    dir = "0704"
     prev_filename = ""
-    data = {dir: {"files":{}}}
+    prev_dir = ""
+    data = {}
     url_dict = {}
 
-    next(classifier_results)
     for row in classifier_results:
         url = row[1]
         classification = row[2]
         filename = row[3]
+        try:
+            dir = get_dir(filename)
+        except:
+            print(row)
+            continue
+
+        if prev_dir == "":
+            d = open('./raw_data_outputs/classifier_results/' + dir + '.json', 'w')
+            data[dir] = {"files":{}}
+            prev_dir = dir
 
         if url != "":
             if prev_filename != filename:
                 if prev_filename != "":
                     url_dict = {"url_count":len(all_urls), "oads_count":len(oads_urls), "oads_urls":list(oads_urls), "non_oads_urls":list(non_oads_urls), "all_urls":sorted(oads_urls.union(non_oads_urls))}
-                    data[dir]["files"][prev_filename] = url_dict
+                    data[prev_dir]["files"][prev_filename] = url_dict
                 all_urls, oads_urls, non_oads_urls = set(), set(), set()
             if classification == "OADS":
                 oads_urls.add(url)
@@ -39,6 +47,13 @@ with open("./classifier_results/" + input_file, newline='') as classifier_result
             all_urls.add(url)
             prev_filename = filename
 
+        if prev_dir != dir:
+            json.dump(data, d)
+            d.close()
+            d = open('./raw_data_outputs/classifier_results/' + dir + '.json', 'w')
+            data = {}
+            data[dir] = {"files":{}}
+            prev_dir = dir
 
     data[dir]["files"][prev_filename] = url_dict
 
--- classifier_results/process_classifier_results.py
+++ classifier_results/process_classifier_results.py
@@ -11,58 +11,60 @@ from util import URLUtil
 util = URLUtil()
 
 # input_file = "test_arxiv_output.csv"
-input_file = "Arxiv-Output.csv"
-output_file = "parsed_" + input_file
+input_files = ["oads_output/part1_Arxiv-Output.csv", "oads_output2/part2_Arxiv-Output.csv"]
 
-revised_output_file = open("./classifier_results/" + output_file, "w")
-revised_output_csv = csv.writer(revised_output_file, delimiter=',')
-revised_output_csv.writerow(['Sentence', 'ValidURL', 'Class', 'File'])
+for input in input_files:
+    output_file = "parsed_" + os.path.basename(input)
 
-with open('raw_data_outputs/classifier_results/' + input_file, newline='') as classifier_results_file:
-    classifier_results = csv.reader(classifier_results_file, delimiter=',')
+    revised_output_file = open("./classifier_results/" + output_file, "w")
+    revised_output_csv = csv.writer(revised_output_file, delimiter=',')
+    # revised_output_csv.writerow(['Sentence', 'ValidURL', 'Class', 'File'])
 
-    prev_file_base = ""
-    prev_data = []
-    max_version = ""
+    with open('raw_data_outputs/' + input, newline='') as classifier_results_file:
+        classifier_results = csv.reader(classifier_results_file, delimiter=',')
 
-    next(classifier_results)
-    for row in classifier_results:
-        sent = row[0]
-        classification = row[1]
-        file_name = row[2]
+        prev_file_base = ""
+        prev_data = []
+        max_version = ""
 
-        try:
-            parsed_file = re.findall(r"(\d{4}.\d*)v(\d*).pdf", file_name)[0]
-        except: 
-            continue
-
-        file_base = parsed_file[0]
-
-        # From Lamia
-        # regex = r'((http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?)'
-        # m = re.findall(regex, sent)
-
-        # Found online 
-        regex2 = r'https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/=]*)'
-        m2 = re.findall(regex2, sent)
-        if m2 != []:
-            u = m2[0].rstrip('.')
+        next(classifier_results)
+        for row in classifier_results:
             try:
-                url = util.validate_url(u)
-            except:
-                url = ''
+                sent = row[0]
+                classification = row[-2]
+                file_name = row[-1]
+                parsed_file = re.findall(r"(\d{4}.\d*)v(\d*).pdf", file_name)[0]
+            except: 
+                print(row)
+                continue
 
-        if prev_file_base != file_base:
-            if prev_file_base != "":
-                revised_output_csv.writerow(prev_data)
-            prev_file_base = file_base
-            max_version = parsed_file[1]
-        else:
-            if int(max_version) < int(parsed_file[1]):
+            file_base = parsed_file[0]
+
+            # From Lamia
+            # regex = r'((http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?)'
+            # m = re.findall(regex, sent)
+
+            # Found online 
+            regex2 = r'https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/=]*)'
+            m2 = re.findall(regex2, sent)
+            if m2 != []:
+                u = m2[0].rstrip('.')
+                try:
+                    url = util.validate_url(u)
+                except:
+                    url = ''
+
+            if prev_file_base != file_base:
+                if prev_file_base != "":
+                    revised_output_csv.writerow(prev_data)
+                prev_file_base = file_base
                 max_version = parsed_file[1]
-        prev_data = [sent, url, classification, file_name]
+            else:
+                if int(max_version) < int(parsed_file[1]):
+                    max_version = parsed_file[1]
+            prev_data = [sent, url, classification, file_name]
 
-    revised_output_csv.writerow(prev_data)
-        
+        revised_output_csv.writerow(prev_data)
+            
 
-revised_output_file.close()
+    revised_output_file.close()
