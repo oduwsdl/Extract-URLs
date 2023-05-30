af3e362070aedc5a05fe8be11596624c708d4df5
Emily Vogt <elvogt@liberty.edu>
2023-05-30 10:31:16 -0400

    Added the file count for each month to the output to help with caculcations

--- classifier_results/urls_per_file.py
+++ classifier_results/urls_per_file.py
@@ -17,7 +17,7 @@ for corpus in corpora:
 
     csv_file = open("./classifier_results/" + prefix + "urls_per_file.csv", "w")
     csvwriter = csv.writer(csv_file)
-    csvwriter.writerow(['Directory', 'URICount', 'Category'])
+    csvwriter.writerow(['Directory', 'URICount', 'Category', 'TotalFiles'])
 
     total_file_count = 0
     total_url_count = 0
@@ -83,13 +83,13 @@ for corpus in corpora:
         oads_count = oads_count - sourceforge_oads_count - bitbucket_oads_count - github_oads_count - gitlab_oads_count
         non_count = non_count - sourceforge_non_count - bitbucket_non_count - github_non_count - gitlab_non_count
 
-        csvwriter.writerow([date, dir_count/file_count, "Total"])
-        csvwriter.writerow([date, oads_count/file_count, "OADS"])
-        csvwriter.writerow([date, non_count/file_count, "NonOADS"])
-        csvwriter.writerow([date, (sourceforge_oads_count + sourceforge_non_count)/file_count, "SourceForge"])
-        csvwriter.writerow([date, (github_oads_count + github_non_count)/file_count, "GitHub"])
-        csvwriter.writerow([date, (gitlab_oads_count + gitlab_non_count)/file_count, "GitLab"])
-        csvwriter.writerow([date, (bitbucket_oads_count + bitbucket_non_count)/file_count, "Bitbucket"])
+        csvwriter.writerow([date, dir_count, "Total", file_count])
+        csvwriter.writerow([date, oads_count, "OADS", file_count])
+        csvwriter.writerow([date, non_count, "NonOADS", file_count])
+        csvwriter.writerow([date, (sourceforge_oads_count + sourceforge_non_count), "SourceForge", file_count])
+        csvwriter.writerow([date, (github_oads_count + github_non_count), "GitHub", file_count])
+        csvwriter.writerow([date, (gitlab_oads_count + gitlab_non_count), "GitLab", file_count])
+        csvwriter.writerow([date, (bitbucket_oads_count + bitbucket_non_count), "Bitbucket", file_count])
     print("Total file count: " + str(total_file_count))
     print("Total URL count: " + str(total_url_count))
     print("SourceForge count: " + str(total_sourceforge_count))
